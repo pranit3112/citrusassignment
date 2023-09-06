392c8cb6eb2a5b07537afadcf620a87795b7480d
Pranit Shirole <62275231+pranit3112@users.noreply.github.com>
2023-09-06 14:50:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// JavaScript for Dropdown Menus
+
 const menu1 = document.getElementById("menu1");
 const menu2 = document.getElementById("menu2");
 
 menu1.addEventListener("change", function () {
     const selectedValue = menu1.value;
-    // You can add logic to perform actions based on the selected value here
+    
     console.log("Selected Value from Menu 1: " + selectedValue);
 });
 
 menu2.addEventListener("change", function () {
     const selectedValue = menu2.value;
-    // You can add logic to perform actions based on the selected value here
+   
     console.log("Selected Value from Menu 2: " + selectedValue);
 });
